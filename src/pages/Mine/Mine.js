e7f10e5cecaf8fed0aad19f340b51b7d28866b4e
chenchen <chenchen@zhiqi.cn>
2018-10-19 09:16:57 +0800

    '今日最新'

--- src/pages/Mine/Mine.js
+++ src/pages/Mine/Mine.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text, ScrollView, ImageBackground, StatusBar, TouchableHighlight } from 'react-native'
 import { ListItem, Divider } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const BARHEIGHT = StatusBar.currentHeight
 
@@ -34,65 +35,59 @@ export default class Mine extends Component {
         <ImageBackground style={{paddingTop: BARHEIGHT, minHeight: 240, backgroundColor: '#fff'}}
                          source={require('../../asset/images/bg.png')}></ImageBackground>
         <View>
-          {
-            list.map((item, i) => (
-              <TouchableHighlight key={i} underlayColor={'#666'} onPress={this.pressItem}>
-                <View>
-                  <ListItem
-                    key={i}
-                    chevron
-                    title={item.title}
-                    leftIcon={{name: item.icon}}
-                  />
-                  {
-                    i !== (list.length - 1) ?
-                      <Divider/> : null
-                  }
-                </View>
-              </TouchableHighlight>
+          {list.map((item, i) => (
+            <TouchableHighlight key={i} underlayColor={'#666'} onPress={this.pressItem}>
+              <View>
+                <ListItem
+                  key={i}
+                  chevron
+                  title={item.title}
+                  leftIcon={{name: item.icon}}
+                />
+                {
+                  i !== (list.length - 1) ?
+                    <Divider/> : null
+                }
+              </View>
+            </TouchableHighlight>
 
-            ))
-          }
+          ))}
         </View>
         <View style={{marginTop: 20}}>
-          {
-            list.map((item, i) => (
-              <TouchableHighlight key={i} underlayColor={'#666'} onPress={this.pressItem}>
-                <View>
-                  <ListItem
-                    key={i}
-                    chevron
-                    title={item.title}
-                    leftIcon={{name: item.icon}}
-                  />
-                  {
-                    i !== (list.length - 1) ?
-                      <Divider/> : null
-                  }
-                </View>
-              </TouchableHighlight>
-            ))
-          }
+          {list.map((item, i) => (
+            <TouchableHighlight key={i} underlayColor={'#666'} onPress={this.pressItem}>
+              <View>
+                <ListItem
+                  key={i}
+                  chevron
+                  title={item.title}
+                  leftIcon={{name: item.icon}}
+                />
+                {
+                  i !== (list.length - 1) ?
+                    <Divider/> : null
+                }
+              </View>
+            </TouchableHighlight>
+          ))}
         </View>
         <View style={{marginTop: 20}}>
-          {
-            list.map((item, i) => (
-              <TouchableHighlight key={i} underlayColor={'#666'} onPress={this.pressItem}>
-                <View>
-                  <ListItem
-                    key={i}
-                    chevron
-                    title={item.title}
-                    leftIcon={{name: item.icon}}
-                  />
-                  {
-                    i !== (list.length - 1) ?
-                      <Divider/> : null
-                  }
-                </View>
-              </TouchableHighlight>
-            ))
-          }
+          {list.map((item, i) => (
+            <TouchableHighlight key={i} underlayColor={'#666'} onPress={this.pressItem}>
+              <View>
+                <ListItem
+                  key={i}
+                  chevron
+                  title={item.title}
+                  leftIcon={{name: item.icon}}
+                />
+                {
+                  i !== (list.length - 1) ?
+                    <Divider/> : null
+                }
+              </View>
+            </TouchableHighlight>
+          ))}
         </View>
       </ScrollView>
     )
