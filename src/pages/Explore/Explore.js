fdcd1873eee825cf08a0ab9da3711dad6754e41a
chenchen <chenchen@zhiqi.cn>
2018-10-24 17:26:26 +0800

    '更换tabview组件'

--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -1,33 +1,45 @@
 import React, { Component } from 'react'
 import { View, StyleSheet, StatusBar } from 'react-native'
-import ScrollableTabView from 'react-native-scrollable-tab-view'
+import { TabView, SceneMap } from 'react-native-tab-view'
 import Find from './Find'
 import Following from './Following'
 import CustomTabBar from './CustomTabBar'
 
 const BARHEIGHT = StatusBar.currentHeight
 export default class Explore extends Component {
+  state = {
+    index: 0,
+    routes: [
+      {key: 'Find', title: '发现'},
+      {key: 'Following', title: '关注'},
+    ],
+  }
   static navigationOptions = {
     title: 'Explore',
+  }
+
+  renderTabBar = (props) => {
+    console.log(props)
+    return <CustomTabBar
+      {...props}
+      backgroundColor='#f4f4f4'
+      tabUnderlineDefaultWidth={30}
+      tabUnderlineScaleX={3}
+      activeColor='#FFA200'
+      inactiveColor='#333'
+    />
   }
 
   render() {
     return (
       <View style={styles.container}>
         <StatusBar barStyle='dark-content' translucent={true} backgroundColor='transparent'/>
-        <ScrollableTabView
+        <TabView
           style={{marginTop: BARHEIGHT}}
-          renderTabBar={() => <CustomTabBar
-            backgroundColor='#f4f4f4'
-            tabUnderlineDefaultWidth={30}
-            tabUnderlineScaleX={3}
-            activeColor='#FFA200'
-            inactiveColor='#333'
-          />}
-        >
-          <Find tabLabel='发现'/>
-          <Following tabLabel='关注'/>
-        </ScrollableTabView>
+          navigationState={this.state}
+          onIndexChange={index => this.setState({index})}
+          renderScene={SceneMap({Find, Following})}
+        />
       </View>
     )
   }
